e3e02548406bfd44e88a83eccab5c274b783f02d
ayberkozgur <ayberk.ozgur@epfl.ch>
2015-07-15 14:58:00 +0200

    Reduced working area for safety

--- CreatrHSProduction20150115/Configuration.h
+++ CreatrHSProduction20150115/Configuration.h
@@ -170,8 +170,8 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 
 #define min_software_endstops true //If true, axis won't move to coordinates less than HOME_POS.
 #define max_software_endstops true  //If true, axis won't move to coordinates greater than the defined lengths below.
-#define X_MAX_LENGTH 270
-#define Y_MAX_LENGTH 285
+#define X_MAX_LENGTH 265
+#define Y_MAX_LENGTH 280
 #define Z_MAX_LENGTH 180
 
 // The position of the homing switches. Use MAX_LENGTH * -0.5 if the center should be 0, 0, 0
